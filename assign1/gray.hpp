904d9918514ab2da2c5384aa1ff429b30a61ffee
Ashwary <ashwaryanand@gmail.com>
2018-01-19 00:30:52 +0530

    Add BGR2Gray function

---
+++ assign1/gray.hpp
@@ -0,0 +1,24 @@
+#ifndef GRAY_H
+#define GRAY_H
+
+#include "headers.hpp"
+
+void gray(Pixel &pixel){
+	int temp;
+	temp = pixel[0]*0.114 + pixel[1]*0.587 + pixel[2]*0.299;
+	pixel[0] = temp;
+	pixel[1] = temp;
+	pixel[2] = temp;
+}
+
+void BGR2GRAY(cv::Mat src,cv::Mat &dst){
+	src.forEach<Pixel>(
+		[](Pixel &pixel,const int*position) -> void
+		{
+			gray(pixel);
+		}
+	);
+	dst = src.clone();
+}
+
+#endif
---
+++ assign1/headers.hpp
@@ -0,0 +1,10 @@
+#ifndef HEADERS_HPP
+#define HEADERS_HPP
+
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+typedef cv::Vec3b Pixel;
+
+#endif
