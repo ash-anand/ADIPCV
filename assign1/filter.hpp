18155a3205e566b8259bb1e8e10201a4d06d60c9
Ashwary <ashwaryanand@gmail.com>
2018-01-19 01:50:17 +0530

    Add Median Blur

--- assign1/filter.hpp
+++ assign1/filter.hpp
@@ -3,6 +3,7 @@
 
 #include "headers.hpp"
 #include <iostream>
+#include <vector>
 
 void GaussianNoise(const cv::Mat src,cv::Mat &dst, double mean, double stdDev){
 	cv::Mat mGaussian_noise = cv::Mat(src.size(),CV_8UC3);
@@ -25,7 +26,7 @@ void GaussianNoise(const cv::Mat src,cv::Mat &dst, double mean, double stdDev){
 }
 
 cv::Mat getGaussian(int _x, int _y,float stdDev){
-	cv::Mat kernel(_x,_y,CV_32FC1);
+	cv::Mat kernel(_x,_y,cv::DataType<double>::type);
 	int x = _x/2;
 	int y = _y/2;
 
@@ -34,36 +35,76 @@ cv::Mat getGaussian(int _x, int _y,float stdDev){
 
 	for(int i = -x; i <= x; i++){
 		for(int j = -y; j <= y; j++){
-			r = sqrt(i*i + j*j);
-			kernel.at<double>(j+2,i+2) = exp(-(r*r)/s)/(CV_PI*s);
-			sum += kernel.at<double>(j+2,i+2);
+			r = i*i + j*j;
+			kernel.at<double>(j+y,i+x) = exp((-r)/s);
+			sum += kernel.at<double>(j+y,i+x);
 		}
 	}
 
-	kernel /= sum;
-	// for (int i = 0; i < kernel.rows; ++i)
-	// {
-	// 	for (int j = 0; j < kernel.cols; ++j)
-	// 	{
-	// 		kernel.at<double>(j,i) /= sum;
-	// 	}
-	// }
+	for (int i = 0; i < kernel.rows; ++i)
+	{
+		for (int j = 0; j < kernel.cols; ++j)
+		{
+			kernel.at<double>(j,i) /= sum;
+		}
+	}
 	return kernel;
 }
 
 void GaussianBlur(const cv::Mat src,cv::Mat &dst,int x, int y , float stdDev){
 	cv::Mat kernel = getGaussian(x,y,stdDev).clone();
-	for (int i = 0; i < src.rows; ++i)
-	{
-		for (int j = 0; j < src.cols; ++j)
-		{
+	dst.create(src.size(),src.type());
+	for (int i = 0; i < src.rows; ++i){
+		for (int j = 0; j < src.cols; ++j){
+			int sum = 0;
 			for(int k = 0; k < kernel.rows; k++){
 				for(int l = 0; l < kernel.cols; l++){
-					
+					int pic_i = i + k - kernel.rows/2;
+					int pic_j = j + l - kernel.cols/2;
+					if( pic_j < 0 || pic_i < 0)
+						continue;
+					sum += kernel.at<double>(k,l)*src.at<uchar>(pic_i,pic_j);
 				}
 			}
+			dst.at<uchar>(i,j) = sum;
 		}
 	}
+}
+
+double median(std::vector<int> scores)
+{
+  double median;
+  size_t size = scores.size();
+
+  sort(scores.begin(), scores.end());
+
+  if (size  % 2 == 0)
+  {
+      median = (scores[size / 2 - 1] + scores[size / 2]) / 2;
+  }
+  else 
+  {
+      median = scores[size / 2];
+  }
+
+  return median;
+}
+
+void MedianBlur(const cv::Mat src, cv::Mat &dst, int _x, int _y){
+	dst.create(src.size(),src.type());
+	for(int i = 0; i < src.rows; i++)
+		for(int j = 0; j < src.cols; j++){
+			std::vector<int> temp;
+			for(int k = 0; k <= _x; k++)
+				for(int l = 0; l <= _y; l++){
+					int pic_i = i + k - _x/2;
+					int pic_j = j + l - _y/2;
+					if( pic_j < 0 || pic_i < 0)
+						continue;
+					temp.push_back(src.at<uchar>(pic_i,pic_j));
+				}
+			dst.at<uchar>(i,j) = median(temp);
+		}
 }
 
 #endif
